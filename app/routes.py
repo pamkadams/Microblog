dcb976dbe169e8f886bd9b99845539d2f584cf8a
pamkadams <46425867+pamkadams@users.noreply.github.com>
2020-05-01 14:46:49 -0400

    login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +23,8 @@ def index():
     ]
     return render_template('index.html', user=user, posts=posts)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
